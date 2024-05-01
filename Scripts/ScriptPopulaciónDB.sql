1217fd43abf2b63620806acc1dbf463cd92f4b47
Gambo2907 <sgamo2220@gmail.com>
2024-04-30 23:12:48 -0600

    Populación DB

---
+++ Scripts/ScriptPopulaciónDB.sql
@@ -0,0 +1,9 @@
+--Populacion estado de los activos
+Insert into Estado_Activos
+Values (1,'Disponible');
+
+Insert into Estado_Activos
+Values (2,'Prestado');
+
+Insert into Estado_Activos
+Values (3,'Averiado');
